c7f913eea8503f177f585a7a0c720338d82b1b42
hlibk <hlibk@proton.me>
2024-04-25 11:49:25 -0600

    Add custom training model using vggface, use pipenv for dependency version management

--- main.py
+++ main.py
@@ -1,37 +1,86 @@
-import tensorflow as tf
-import keras
 from keras_vggface.vggface import VGGFace
-import mtcnn
+from mtcnn.mtcnn import MTCNN
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
 import numpy as np
-import matplotlib as mpl
-import PIL
-import os
-import os.path
+import os, sys
+from pathlib import Path
+import keras
+import tensorflow
 
-train_dataset = keras.utils.image_dataset_from_directory("./faces", image_size=(640,480))
+def resize_face(path, pathtosave):
+    for item in os.listdir(path):
+        if os.path.isfile(path+"/"+item):
+            img=cv2.imread(path+"/"+item, flags=cv2.IMREAD_UNCHANGED)
+            img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            detector = MTCNN()
+            results = detector.detect_faces(img)
+            if results:
+                x1, y1, width, height = results[0]['box']
+                x2, y2 = x1 + width, y1 + height
+                face = img[y1:y2, x1:x2]
+                image = Image.fromarray(face)
+                image = image.resize((224, 224))
+                Path(pathtosave+"/").mkdir(parents=True, exist_ok=True)
+                image.save(pathtosave+"/"+item, "PNG", quality=100)
+            else:
+                print("Face not detected!")
+
+def extract_face(facefile):
+    img=cv2.imread(facefile, flags=cv2.IMREAD_UNCHANGED)
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    detector = MTCNN()
+    results = detector.detect_faces(img)
+    x1, y1, width, height = results[0]['box']
+    x2, y2 = x1 + width, y1 + height
+    face = img[y1:y2, x1:x2]
+    image = Image.fromarray(face)
+    image = image.resize((224, 224))
+    face_array = np.asarray(image)
+    return face_array
+
+def create_resized_faces():
+    path = "./faces"
+    dirs = os.listdir(path)
+
+    for subdir in dirs:
+        print(f"resizing {subdir}")
+        resize_face(path+f"/{subdir}", f"./faces_resized/{subdir}")
+
+train_dataset = keras.utils.image_dataset_from_directory(
+    "./faces_resized",
+    shuffle=True,
+    batch_size=3,
+    image_size=(224,224)
+)
 
 data_augmentation = keras.Sequential([
     keras.layers.RandomFlip("horizontal"),
-    keras.layers.RandomRotation(0.2),
+    keras.layers.RandomRotation(0.2)
 ])
 
-vggface = VGGFace(model="vgg16", include_top=False, input_shape=(640,480,3))
+model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3))
 
-nb_class = 20
+nb_class = 19
 
-vggface.trainable = False
-# last_layer = vggface.get_layer("avg_pool").output
+model.trainable = False
+last_layer = model.get_layer("avg_pool").output
 
-inputs = tf.keras.Input(shape=(640,480,3))
+inputs = tensorflow.keras.Input(shape=(224,224,3))
+
 x = data_augmentation(inputs)
-x = vggface(x)
+x = model(x)
 x = keras.layers.Flatten(name="flatten")(x)
+
 out = keras.layers.Dense(nb_class, name="classifier")(x)
-custom_vgg = keras.Model(inputs, out)
+custom_vgg_model = keras.Model(inputs, out)
 
-learning_rate = 0.0001
-custom_vgg.compile(optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate),
-                   loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                   metrics=["accuracy"])
+base_learning_rate = 0.001
 
-history = custom_vgg.fit(train_dataset, epochs=20)
+custom_vgg_model.compile(
+    optimizer=tensorflow.keras.optimizers.Adam(learning_rate=base_learning_rate),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"]
+)
+history = custom_vgg_model.fit(train_dataset, epochs=20)
